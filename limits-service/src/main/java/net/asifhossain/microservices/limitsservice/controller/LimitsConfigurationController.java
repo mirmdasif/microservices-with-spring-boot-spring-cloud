eba69242317577e8ef9c85036c69d136db19e740
Mir Md. Asif Hossain <mir.md.asif@gmail.com>
2018-08-19 18:27:10 +0600

    Step 03: Configur limit service to read limit configuration from properties

--- limits-service/src/main/java/net/asifhossain/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ limits-service/src/main/java/net/asifhossain/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -1,15 +1,20 @@
 package net.asifhossain.microservices.limitsservice.controller;
 
 import net.asifhossain.microservices.limitsservice.bean.LimitConfiguration;
+import net.asifhossain.microservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public LimitConfiguration retriveLimitsFromConfigurations() {
 
-        return new LimitConfiguration(1, 1000);
+        return new LimitConfiguration(configuration.getMinimum(), configuration.getMaximum());
     }
 }
