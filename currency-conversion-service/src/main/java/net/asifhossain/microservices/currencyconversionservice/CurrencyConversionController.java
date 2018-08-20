dc4dac5d40563df7e383e47b1113c45e4e6306b0
Mir Md. Asif Hossain <mir.md.asif@gmail.com>
2018-08-20 16:46:56 +0600

    Step 21: Using feign rest client for service invokation

--- currency-conversion-service/src/main/java/net/asifhossain/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/net/asifhossain/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,33 +1,26 @@
 package net.asifhossain.microservices.currencyconversionservice;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
+
+    private CurrencyExchangeServiceProxy currencyExchangeServiceProxy;
+
+    public CurrencyConversionController(CurrencyExchangeServiceProxy currencyExchangeServiceProxy) {
+        this.currencyExchangeServiceProxy = currencyExchangeServiceProxy;
+    }
 
     @GetMapping("currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionBean convertCurrency(@PathVariable String from,
                                                   @PathVariable String to,
                                                   @PathVariable BigDecimal quantity) {
 
-        Map<String, String> uriVariables = new HashMap<>();
-
-        uriVariables.put("from", from);
-        uriVariables.put("to", to);
-
-        ResponseEntity<CurrencyConversionBean> responseEntity =
-                new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
-                CurrencyConversionBean.class, uriVariables);
-
-        CurrencyConversionBean conversionEntity = responseEntity.getBody();
+        CurrencyConversionBean conversionEntity = currencyExchangeServiceProxy.retrieveExchangeValue(from, to);
 
         return new CurrencyConversionBean(1L, from, to, conversionEntity.getConversionMultiple(), quantity,
                 quantity.multiply(conversionEntity.getConversionMultiple()), conversionEntity.getPort());
---
+++ currency-conversion-service/src/main/java/net/asifhossain/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,13 @@
+package net.asifhossain.microservices.currencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "currency-conversion-service", url = "localhost:8000")
+public interface CurrencyExchangeServiceProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
+
+}
